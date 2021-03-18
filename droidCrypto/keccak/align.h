ea5348e2f62b66cf2c49ea3db87cb6ebcace46a9
Daniel Kales <daniel.kales@gmail.com>
2021-03-18 18:20:32 +0100

    rename macro to avoid clashes

--- droidCrypto/keccak/align.h
+++ droidCrypto/keccak/align.h
@@ -14,19 +14,20 @@ http://creativecommons.org/publicdomain/zero/1.0/
 #ifndef _align_h_
 #define _align_h_
 
-/* on Mac OS-X and possibly others, ALIGN(x) is defined in param.h, and -Werror chokes on the redef. */
-#ifdef ALIGN
-#undef ALIGN
+/* on Mac OS-X and possibly others, ALIGN(x) is defined in param.h, and -Werror
+ * chokes on the redef. */
+#ifdef ATTR_ALIGN
+#undef ATTR_ALIGN
 #endif
 
 #if defined(__GNUC__)
-#define ALIGN(x) __attribute__ ((aligned(x)))
+#define ATTR_ALIGN(x) __attribute__((aligned(x)))
 #elif defined(_MSC_VER)
-#define ALIGN(x) __declspec(align(x))
+#define ATTR_ALIGN(x) __declspec(align(x))
 #elif defined(__ARMCC_VERSION)
-#define ALIGN(x) __align(x)
+#define ATTR_ALIGN(x) __align(x)
 #else
-#define ALIGN(x)
+#define ATTR_ALIGN(x)
 #endif
 
 #endif
